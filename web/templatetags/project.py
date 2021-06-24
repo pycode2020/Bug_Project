a7b64d10c7174febd0072f4a012bda6b538e9187
icode2020 <icode2020@protonmail.com>
2021-06-24 13:09:09 +0800

    项目管理-项目菜单默认选中

--- web/templatetags/project.py
+++ web/templatetags/project.py
@@ -1,5 +1,6 @@
 from django.template import Library
 from web import models
+from django.urls import reverse
 
 register = Library()
 
@@ -12,3 +13,23 @@ def all_project_list(request):
     join_project_list = models.ProjectUser.objects.filter(user=request.project.user)
 
     return {'my': my_project_list, 'join': join_project_list}
+
+
+@register.inclusion_tag('inclusion/manage_memu_list.html')
+def manage_memu_list(request):
+    data_list = [
+        {'title': '概览', 'url': reverse('manage_dashboard', kwargs={'project_id': request.project.project.id})},
+        {'title': '问题', 'url': reverse('manage_issues', kwargs={'project_id': request.project.project.id})},
+        {'title': '统计', 'url': reverse('manage_statistics', kwargs={'project_id': request.project.project.id})},
+        {'title': '文件', 'url': reverse('manage_file', kwargs={'project_id': request.project.project.id})},
+        {'title': '文档', 'url': reverse('manage_wiki', kwargs={'project_id': request.project.project.id})},
+        {'title': '设置', 'url': reverse('manage_setting', kwargs={'project_id': request.project.project.id})},
+    ]
+    
+    for item in data_list:
+        if request.path_info.startswith(item['url']):
+            item['class'] = 'active'
+
+    return {'data_list': data_list}
+
+
